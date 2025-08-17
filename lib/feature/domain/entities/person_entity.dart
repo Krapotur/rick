89a3a6df91cdb3f7f5bebfd319343617e463dafe
Krapotur <68387294+Krapotur@users.noreply.github.com>
2025-08-17 13:46:56 +0300

    Add project

---
+++ lib/feature/domain/entities/person_entity.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+
+class PersonEntity extends Equatable {
+  const PersonEntity({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.type,
+    required this.gender,
+    required this.origin,
+    required this.location,
+    required this.image,
+    required this.episode,
+    required this.created,
+  });
+
+  final int id;
+  final String name;
+  final String status;
+  final String type;
+  final String gender;
+  final LocationEntity origin;
+  final LocationEntity location;
+  final String image;
+  final List<String> episode;
+  final DateTime created;
+
+  @override
+  List<Object> get props => [
+        id,
+        name,
+        status,
+        type,
+        gender,
+        origin,
+        location,
+        image,
+        episode,
+        created,
+      ];
+}
+
+class LocationEntity {
+  final String name;
+  final String url;
+
+  const LocationEntity({required this.name, required this.url});
+}
